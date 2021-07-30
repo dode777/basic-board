45e977bb3875dcb840bba96fb787fe161f5083c9
Dohae <dode777.dev@gmail.com>
2021-07-30 11:18:50 +0900

    Feat: View
    Node.js와 Express를 이용해 게시판 만들기 ⑤ - View 만들기

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const path = require('path');
 require('./db');
 
 const PORT = process.env.PORT || 3000;
@@ -13,6 +14,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(morgan('dev'));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.listen(PORT, () => {
   console.log(`✅ Listening on 'http://localhost:${PORT}'`);
 });
